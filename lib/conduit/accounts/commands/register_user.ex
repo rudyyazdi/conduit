09dc3d399f22abe77a478267247986737de2c315
Ben Smith <ben@10consulting.com>
2017-06-21 14:26:42 +0100

    Email address validation

--- lib/conduit/accounts/commands/register_user.ex
+++ lib/conduit/accounts/commands/register_user.ex
@@ -1,9 +1,9 @@
 defmodule Conduit.Accounts.Commands.RegisterUser do
   defstruct [
-    :uuid,
-    :username,
-    :email,
-    :hashed_password,
+    uuid: "",
+    username: "",
+    email: "",
+    hashed_password: "",
   ]
 
   use ExConstructor
@@ -12,12 +12,19 @@ defmodule Conduit.Accounts.Commands.RegisterUser do
   alias Conduit.Accounts.Commands.RegisterUser
 
   validates :uuid, uuid: true
+
   validates :username,
     presence: [message: "can't be empty"],
     format: [with: ~r/^[a-z0-9]+$/, allow_nil: true, allow_blank: true, message: "is invalid"],
     string: true,
     unique_username: true
-  validates :email, presence: [message: "can't be empty"], string: true
+
+  validates :email,
+    presence: [message: "can't be empty"],
+    format: [with: ~r/\S+@\S+\.\S+/, allow_nil: true, allow_blank: true, message: "is invalid"],
+    string: true,
+    unique_email: true
+
   validates :hashed_password, presence: [message: "can't be empty"], string: true
 
   @doc """
@@ -33,10 +40,18 @@ defmodule Conduit.Accounts.Commands.RegisterUser do
   def downcase_username(%RegisterUser{username: username} = register_user) do
     %RegisterUser{register_user | username: String.downcase(username)}
   end
+
+  @doc """
+  Convert email address to lowercase characters
+  """
+  def downcase_email(%RegisterUser{email: email} = register_user) do
+    %RegisterUser{register_user | email: String.downcase(email)}
+  end
 end
 
 defimpl Conduit.Validation.Middleware.Uniqueness.UniqueFields, for: Conduit.Accounts.Commands.RegisterUser do
   def unique(_command), do: [
+    {:email, "has already been taken"},
     {:username, "has already been taken"},
   ]
 end
