62147e5f6c5c6fe8bc22fe765803e97dbfb1218a
Ben Smith <ben@10consulting.com>
2017-06-22 14:21:54 +0100

    User controller web tests

---
+++ test/support/storage.ex
@@ -0,0 +1,40 @@
+defmodule Conduit.Storage do
+  @doc """
+  Clear the event store and read store databases
+  """
+  def reset! do
+    :ok = Application.stop(:conduit)
+    :ok = Application.stop(:commanded)
+    :ok = Application.stop(:eventstore)
+
+    reset_eventstore()
+    reset_readstore()
+
+    {:ok, _} = Application.ensure_all_started(:conduit)
+  end
+
+  defp reset_eventstore do
+    eventstore_config = Application.get_env(:eventstore, EventStore.Storage)
+
+    {:ok, conn} = Postgrex.start_link(eventstore_config)
+
+    EventStore.Storage.Initializer.reset!(conn)
+  end
+
+  defp reset_readstore do
+    readstore_config = Application.get_env(:conduit, Conduit.Repo)
+
+    {:ok, conn} = Postgrex.start_link(readstore_config)
+
+    Postgrex.query!(conn, truncate_readstore_tables(), [])
+  end
+
+  defp truncate_readstore_tables do
+"""
+TRUNCATE TABLE
+  accounts_users,
+  projection_versions
+RESTART IDENTITY;
+"""
+  end
+end
